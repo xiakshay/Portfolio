3702bce60b650ae42af45d5daf7855b497c1c7b5
Akshay01-M <akshay010175@gmail.com>
2021-08-11 20:43:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My portfolio
+## All updates will be here related to my carrier
---
+++ script/frontscript.js
@@ -0,0 +1,33 @@
+const imgBx = document.querySelector('.imgBx');
+const slides = imgBx.getElementsByTagName('img');
+var i=0;
+
+function nextSlide(){
+    slides[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+}
+
+function prevSlide(){
+    slides[i].classList.remove('active');
+    i = (i - 1 + slides.length) % slides.length;
+    slides[i].classList.add('active');
+}
+
+
+
+const contentBx = document.querySelector('.contentBx');
+const slidesText = contentBx.getElementsByTagName('div');
+var j=0;
+
+function nextSlideText(){
+    slidesText[j].classList.remove('active');
+    j = (j + 1) % slidesText.length;
+    slidesText[i].classList.add('active');
+}
+
+function prevSlideText(){
+    slidesText[j].classList.remove('active');
+    j = (j - 1 + slidesText.length) % slidesText.length;
+    slidesText[j].classList.add('active');
+}
